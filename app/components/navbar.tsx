673e1ebb4b081431600905c85439e67e5ee6a8d2
Raghad Hav <raghad.hav@gmail.com>
2024-04-14 09:39:22 +0100

    create project structure and add navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-20 top-0 flex justify-between items-center px-4">
+      <div>
+        <img src="/images/sct-logo.png" alt="Logo" className="h-20 w-auto" />
+      </div>
+      <ul className="flex gap-x-6 text-black">
+        <li>
+          <Link href="/">
+            <p>Home</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/courses">
+            <p>Courses</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/stories">
+            <p>Success Stories</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contacts">
+            <p>Contacts</p>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
