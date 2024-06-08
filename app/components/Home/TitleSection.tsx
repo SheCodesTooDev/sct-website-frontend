b95c201862d3eda514c6b515eaadd284fee4fa66
Raghad al-Abboodi <raghad.hav@gmail.com>
2024-06-08 10:38:32 +0100

    delete unused files and reorginise code (#8)

---
+++ app/components/Home/TitleSection.tsx
@@ -0,0 +1,10 @@
+import styles from "../style/Home.module.css";
+const TitleSection = () => {
+  return (
+    <div>
+      <h1 className={styles.inspire}>Inspiring Women To Code;</h1>
+    </div>
+  );
+};
+
+export default TitleSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,12 @@
-import Empowering from './components/empowering/Empowering';
-import './components/style/home.css'
-
+import EmpoweringSection from "./components/Home/EmpoweringSection";
+import ExploreSection from "./components/Home/ExploreSection";
+import TitleSection from "./components/Home/TitleSection";
 export default function Home() {
   return (
     <>
-      <div>
-        
-        
-        <h1 className="inspir">Inspiring Women To Code;</h1>
-        <h3 className='explore'>Explore the programming areas of interest you desire.</h3>
-        <div className="home-container">
-        <div className='bag-green'>
-
-
-
-</div>
-
-        <button className='exp-butt'>Explore our Courses</button>
-        <div className="home-images">
-          <div className="cul-one">
-          <img src="/images/1.jpg" alt="Image 1" />
-        <img src="/images/2.jpg" alt="Image 2" />
-
-          </div>
-          <div className="cul-two">
-          <img src="/images/3.jpg" alt="Image 3" />
-        <img src="/images/4.jpg" alt="Image 4" />
-
-          </div>
-       
-        <div className="cul-three">
-        <img src="/images/5.jpg" alt="Image 5" />
-
-<img src="/images/6.jpg" alt="Image 6" />
-
-        </div>
-        <div className="cul-fuor">
-        <img src="/images/7 .jpeg" alt="Image 7" />
-
-<img src="/images/8 .jpeg" alt="Image 8" />
-        </div>
-        <div className="cul-five">
-        <img src="/images/9.jpeg" alt="Image 9" />
-        <img src="/images/10 .jpeg" alt="Image 10" />
-
-        </div>
-        
-
-       
-
-       
-
-        </div>
-
-
-
-        </div>
-        {/* <button className='exp-butt'>Explore our Courses</button> */}
-       
-
-
-        
-
-        
-
-      
-
-
-
-
-       
-
-        </div>
-        <Empowering/>
-
+      <TitleSection />
+      <ExploreSection />
+      <EmpoweringSection />
     </>
   );
 }
