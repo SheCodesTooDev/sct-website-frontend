bd5c1d043b856c12c1903d438e51a2ead4f3742a
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2024-08-10 13:04:21 +0300

    fix empowreing and add new navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,34 +1,23 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Logo from '../../assets/logos/sct.svg';
+import Menu from '../../assets/logos/menu.svg';
+
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className="w-full h-20 top-0 flex justify-between items-center px-4">
-      <div>
-        <img src="/images/sct-logo.png" alt="Logo" className="h-20 w-auto" />
-      </div>
-      <ul className="flex gap-x-6 text-black">
-        <li>
-          <Link href="/">
-            <p>Home</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/courses">
-            <p>Courses</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/success-stories">
-            <p>Success Stories</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contacts">
-            <p>Contacts</p>
-          </Link>
-        </li>
-      </ul>
+    <div className='py-4 px-2 md:px-8 lg:px-36 flex justify-between items-center'>
+      <Image className=' w-16 h-8 md:w-auto md:h-auto' src={Logo} alt="'logo" />
+      <span className='hidden md:flex gap-8 lg:text-lg	'>
+        <Link href='/'>Home</Link>
+        <Link href='/services'>Services</Link>
+        <Link href='/stories'>Stories</Link>
+        <Link href='/contact'>Contact Us</Link>
+      </span>
+      <span className='flex md:hidden'>
+        <Image src={Menu} alt='menu' />
+      </span>
     </div>
   );
 };
