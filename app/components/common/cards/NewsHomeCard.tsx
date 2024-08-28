2417a51f189b06d69bde8250bc02f79c349d2031
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2024-08-28 04:34:41 +0300

    fix home news cards

--- app/components/common/cards/NewsHomeCard.tsx
+++ app/components/common/cards/NewsHomeCard.tsx
@@ -3,20 +3,20 @@ import Link from 'next/link';
 
 const NewsHomeCard = ({ photo, title, date, slug }: any) => {
   return (
-    <div className='flex flex-col items-start px-0 md:px-10 xl:px-0 justify-center text-start mb-10'>
-      <div className=' grid  grid-cols-12 xl:grid-cols-2 gap-x-4'>
-        <div className=' col-span-6 xl:col-span-1 relative w-full md:w-72 lg:w-full  xl:w-52 h-32'>
-          <Image
-            className='rounded-xl w-full h-32 md:w-72 lg:w-full  xl:w-52 object-cover '
-            src={photo}
-            alt='illustration'
-            layout='fill'
-          />
-        </div>
+    <div className='flex flex-col items-start px-0 md:px-10 xl:px-0 justify-center text-start mb-5'>
+      <div className='flex gap-8 '>
+        <Image
+          src={photo}
+          width='0'
+          height='0'
+          sizes='100vw'
+          className='rounded-xl w-24 h-24 md:w-40 xl:w-60 xl:h-36 object-cover'
+          alt='news'
+        />
 
-        <div className=' col-span-6 xl:col-span-1 flex flex-col justify-start items-start gap-3'>
+        <div className='flex flex-col justify-start items-start gap-3'>
           <p className='font-medium text-md'>{title}</p>
-          <p className='font-medium text-xs mb-3 text-secondary'> 📅 {date}</p>
+          <p className='font-medium text-xs text-secondary'> 📅 {date}</p>
           <Link
             href={`/news/${slug}`}
             className='text-sctblue text-sm  underline flex justify-end'
