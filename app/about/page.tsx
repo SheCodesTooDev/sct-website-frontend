51fba26046b3f84a3a1f34383f9ec893ee3a3c2d
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2024-08-29 09:08:31 +0300

    add about hero

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutHero from '../components/about/AboutHero';
+
+const About = () => {
+  return (
+    <div className='sct-bg flex flex-col w-full justify-center items-center py-20 px-8 xl:px-32 text-start'>
+      <AboutHero />
+    </div>
+  );
+};
+
+export default About;
---
+++ app/components/about/AboutHero.tsx
@@ -0,0 +1,27 @@
+import YellowLine from '../common/YellowLine';
+
+const AboutHero = () => {
+  return (
+    <>
+      <div className='green-square'></div>
+      <p className='font-bold text-4xl leading-loose text-sctblue '>About </p>
+
+      <YellowLine />
+      <p className='font-medium text-start text-base md:text-md mb-16  leading-loose text-black'>
+        She Codes Too is dedicated to advancing the education and career
+        prospects of women in Iraq and the United Kingdom within the field of
+        technology. Our primary objective is to empower women by providing them
+        with the skills, knowledge, and opportunities necessary to succeed in
+        the tech industry. We create programs to teach women over a period of 6
+        or 7 months through and online and in-person classes. Through a variety
+        of courses related to computer programming, the participants learn about
+        their desired topic or area. Beside teaching technical skills, we also
+        provide mentoring and instruction in soft skills and career development
+        to reach maximum benefits for the participants and enable them to secure
+        tech jobs.
+      </p>
+    </>
+  );
+};
+
+export default AboutHero;
