68de1c681261adcd0df74b704691eaef391042e0
Raghad Hav <raghad.hav@gmail.com>
2024-09-16 17:38:14 +0100

    fix config file

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     API_URL: process.env.API_URL,
   },
   images: {
-    domains: ['process','env','DOMAIN'],
+    domains: [process.env.DOMAIN],
   },
   reactStrictMode: false,
 };
