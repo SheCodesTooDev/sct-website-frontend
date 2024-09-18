e193a4aaf9b40581218b10f0d6541b23e7d8afbb
amna mohamed <81439598+amna-201@users.noreply.github.com>
2024-09-18 16:13:45 +0300

    edit homepage content (#23) (#24)
    Co-authored-by: Raghad al-Abboodi <raghad.hav@gmail.com>

--- app/components/Home/InquirySection.tsx
+++ app/components/Home/InquirySection.tsx
@@ -1,18 +1,21 @@
-import Button from '../common/Button';
-import Line from '../../assets/illustrations/line.svg';
-import Image from 'next/image';
+import Button from "../common/Button";
+import Line from "../../assets/illustrations/line.svg";
+import Image from "next/image";
+import Link from "next/link";
 const InquirySection = () => (
-  <div className='bg-lightblue flex flex-col w-full justify-center items-center py-20 px-5 text-center'>
-    <p className='font-medium text-2xl md:text-3xl leading-loose'>
+  <div className="bg-lightblue flex flex-col w-full justify-center items-center py-20 px-5 text-center">
+    <p className="font-medium text-2xl md:text-3xl leading-loose">
       Have questions or need assistance? <br /> Get in touch with us today!
     </p>
 
-    <div className='flex justify-center my-2'>
-      <Image src={Line} alt='line' />
+    <div className="flex justify-center my-2">
+      <Image src={Line} alt="line" />
     </div>
 
-    <div className='flex justify-center'>
-      <Button color='bg-sctyellow'>Contact Us</Button>
+    <div className="flex justify-center">
+      <Link href="contact">
+        <Button color="bg-sctyellow">Contact Us</Button>
+      </Link>
     </div>
   </div>
 );
