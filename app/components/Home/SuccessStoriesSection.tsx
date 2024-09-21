17a11b91f9437609b0cf260bb6e65ca177aef837
Raghad Hav <raghad.hav@gmail.com>
2024-09-21 14:03:33 +0100

    add boolean for if course is close or open

--- app/components/Home/SuccessStoriesSection.tsx
+++ app/components/Home/SuccessStoriesSection.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import Blueline from '../common/BlueLine';
-import { getSCTStories } from '@/app/services/api';
-import HomeStoryCard from '../common/cards/HomeStoryCard';
+import Image from "next/image";
+import Blueline from "../common/BlueLine";
+import { getSCTStories } from "@/app/services/api";
+import HomeStoryCard from "../common/cards/HomeStoryCard";
 
 interface storyProps {
   children: React.ReactNode;
@@ -12,20 +12,21 @@ interface storyProps {
 const SuccessStories = async () => {
   const stories = await getSCTStories();
   if (!Array.isArray(stories)) {
-    console.error('Expected an array of stories, but received:', stories);
+    console.error("Expected an array of stories, but received:", stories);
     return <div>Error loading stories.</div>;
   }
   const story = stories[Math.floor(Math.random() * stories.length)];
 
   return (
-    <div className=' flex flex-col w-full justify-center items-center py-20 px-5 text-center'>
-      <p className='font-medium text-3xl leading-loose'>Success Stories</p>
+    <div className=" flex flex-col w-full justify-center items-center py-20 px-5 text-center">
+      <p className="font-medium text-3xl leading-loose">Success Stories</p>
       <Blueline />
-      <p className='font-medium text-lg leading-loose text-secondary w-60 sm:w-72 md:w-full'>
-        Several selected alumni, who already believe in SheCodesToo.
+      <p className="font-medium text-lg leading-loose text-secondary w-60 sm:w-72 md:w-full">
+        Meet some of our remarkable alumni, empowered through She Codes Too to
+        achieve their goals. Read their inspiring stories.
       </p>
 
-      <div className='xl:mx-16'>
+      <div className="xl:mx-16">
         <HomeStoryCard slug={story?.slug} />
       </div>
     </div>
