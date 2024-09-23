423cd7b9ba921267f7cf9bd6fcb6a7d0f2514b2b
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2024-09-23 03:32:51 +0300

    fix stories and buttons in home page

--- app/components/common/cards/HomeStoryCard.tsx
+++ app/components/common/cards/HomeStoryCard.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import Student from "@/public/images/8 .jpeg";
-import Link from "next/link";
-import Button from "@/app/components/common/Button";
-import { getSCTStories, getSCTStory } from "@/app/services/api";
-import { truncateText } from "@/app/utils/truncateText";
+import Image from 'next/image';
+import Student from '@/public/images/8 .jpeg';
+import Link from 'next/link';
+import Button from '@/app/components/common/Button';
+import { getSCTStories, getSCTStory } from '@/app/services/api';
+import { truncateText } from '@/app/utils/truncateText';
 
 interface storyProps {
   slug: String;
@@ -16,43 +16,43 @@ const HomeStoryCard: React.FC<storyProps> = async ({ slug }) => {
   const positions = [
     {
       className:
-        "absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-6",
-      alt: "top-center-image",
+        'absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-6',
+      alt: 'top-center-image',
     },
     {
       className:
-        "absolute top-8 left-1/4 transform -translate-x-2/3 -translate-y-1/3",
-      alt: "top-left-image",
+        'absolute top-8 left-1/4 transform -translate-x-2/3 -translate-y-1/3',
+      alt: 'top-left-image',
     },
     {
       className:
-        "absolute top-8 right-1/4 transform translate-x-2/3 -translate-y-1/3",
-      alt: "top-right-image",
+        'absolute top-8 right-1/4 transform translate-x-2/3 -translate-y-1/3',
+      alt: 'top-right-image',
     },
     {
       className:
-        "absolute top-1/2 left-0 transform -translate-x-6 -translate-y-2/4",
-      alt: "left-center-image",
+        'absolute top-1/2 left-0 transform -translate-x-6 -translate-y-2/4',
+      alt: 'left-center-image',
     },
     {
       className:
-        "absolute bottom-8 left-1/4 transform -translate-x-2/3 translate-y-1/3",
-      alt: "bottom-left-image",
+        'absolute bottom-8 left-1/4 transform -translate-x-2/3 translate-y-1/3',
+      alt: 'bottom-left-image',
     },
     {
       className:
-        "absolute bottom-8 right-1/4 transform translate-x-2/3 translate-y-1/3",
-      alt: "bottom-right-image",
+        'absolute bottom-8 right-1/4 transform translate-x-2/3 translate-y-1/3',
+      alt: 'bottom-right-image',
     },
     {
       className:
-        "absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-6",
-      alt: "bottom-center-image",
+        'absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-6',
+      alt: 'bottom-center-image',
     },
     {
       className:
-        "absolute top-1/2 right-0 transform translate-x-6 -translate-y-2/4",
-      alt: "right-center-image",
+        'absolute top-1/2 right-0 transform translate-x-6 -translate-y-2/4',
+      alt: 'right-center-image',
     },
   ];
 
@@ -61,29 +61,29 @@ const HomeStoryCard: React.FC<storyProps> = async ({ slug }) => {
   }
 
   return (
-    <div className="bg-gray-200 mt-10 w-full border border-t-1 rounded-lg shadow-md py-2 px-6 md:px-10 lg:px-16 xl:px-16 justify-center items-center md:grid md:grid-cols-2 xl:grid-cols-3">
-      <section className="order-1">
-        <div className="relative w-64 h-64 md:w-58 md:h-58 xl:w-80 xl:h-80 mt-10 mx-auto">
+    <div className='bg-gray-200 mt-10 w-full border border-t-1 rounded-lg shadow-md py-2 px-6 md:px-10 lg:px-16 xl:px-16 justify-center items-center md:grid md:grid-cols-2 xl:grid-cols-3'>
+      <section className='order-1'>
+        <div className='relative w-64 h-64 md:w-58 md:h-58 xl:w-80 xl:h-80 mt-10 mx-auto'>
           <Image
-            className="success-img object-contain"
+            className='success-img object-contain'
             src={story.photo}
-            layout="fill"
-            alt="illustration"
+            layout='fill'
+            alt='illustration'
           />
         </div>
       </section>
-      <section className="order-3 xl:px-5 xl:order-2 col-span-2 xl:col-span-1 px-0 md:px-16 lg:px-0 flex flex-col justify-center">
-        <p className="mt-10 font-medium text-xl sm:text-2xl leading-loose text-start">
+      <section className='order-3 xl:px-5 xl:order-2 col-span-2 xl:col-span-1 px-0 md:px-16 lg:px-0 flex flex-col justify-center'>
+        <p className='mt-10 font-medium text-xl sm:text-2xl leading-loose text-start'>
           {story.title}
         </p>
         <div>
           {story.content.map((block: any, index: any) => (
             <p
               key={index}
-              className="mt-2 text-start font-medium text-sm sm:text-md md:text-base w-full leading-loose text-secondary"
+              className='mt-2 text-start font-medium text-sm sm:text-md md:text-base w-full leading-loose text-secondary'
             >
-              {block.type === "heading" &&
-                block.level === 1 &&
+              {block.type === 'heading' &&
+                block.level === 5 &&
                 block.children.map((child: any, idx: any) => {
                   return truncateText(child.text, 200);
                 })}
@@ -92,14 +92,14 @@ const HomeStoryCard: React.FC<storyProps> = async ({ slug }) => {
         </div>
         <Link
           href={`/success-stories/${story.slug}`}
-          className="text-sctblue underline flex justify-end mt-4"
+          className='text-sctblue underline flex justify-end mt-4'
         >
           Read more -&gt;
         </Link>
       </section>
 
-      <section className="order-2 flex items-center xl:order-3">
-        <div className="relative w-64 h-64 mx-auto mt-12">
+      <section className='order-2 flex items-center xl:order-3'>
+        <div className='relative w-64 h-64 mx-auto mt-12'>
           {positions.map((position, index) => (
             <Image
               key={index}
@@ -112,10 +112,10 @@ const HomeStoryCard: React.FC<storyProps> = async ({ slug }) => {
           ))}
         </div>
       </section>
-      <section className="order-last col-span-2 xl:col-span-3">
-        <div className="flex justify-center mt-12 mb-8">
-          <Link href="success-stories">
-            <Button extrStyles="px-2 text-sm" color="bg-sctyellow">
+      <section className='order-last col-span-2 xl:col-span-3'>
+        <div className='flex justify-center mt-12 mb-8'>
+          <Link href='success-stories'>
+            <Button extrStyles='px-2 text-sm' color='bg-sctyellow'>
               More Stories
             </Button>
           </Link>
