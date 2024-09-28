f73cb6771f72b9be72f0bbe3d9916612d744a025
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2024-09-28 05:45:58 +0300

    add animation to courses

--- app/components/courses/CoursesHero.tsx
+++ app/components/courses/CoursesHero.tsx
@@ -1,17 +1,54 @@
+'use client';
+import { motion } from 'framer-motion';
 import YellowLine from '../common/YellowLine';
 
 const CoursesHero = () => {
+  const textVariants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.8, ease: 'easeInOut' },
+    },
+  };
+
+  const subTextVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, ease: 'easeInOut' },
+    },
+  };
+
+  const lineVariants = {
+    visible: {
+      transition: { duration: 0.8, ease: 'easeInOut' },
+    },
+  };
+
   return (
     <>
-      <p className='font-bold text-4xl leading-loose text-sctblue'>
+      <motion.p
+        className='font-bold text-4xl leading-loose text-sctblue'
+        initial='hidden'
+        animate='visible'
+        variants={textVariants}
+      >
         Our Courses
-      </p>
+      </motion.p>
 
       <YellowLine />
-      <div className='font-medium text-lg md:text-md mb-16  leading-loose text-secondary w-60 sm:w-72 md:w-full'>
-        Empowering Growth: Hands-On Workshops and Expert-Led Courses at <br />{' '}
+
+      <motion.div
+        className='font-medium text-lg md:text-md mb-16 leading-loose text-secondary w-60 sm:w-72 md:w-full'
+        initial='hidden'
+        animate='visible'
+        variants={subTextVariants}
+      >
+        Empowering Growth: Hands-On Workshops and Expert-Led Courses at <br />
         <p className='font-bold text-black'>She Codes Too.</p>
-      </div>
+      </motion.div>
     </>
   );
 };
