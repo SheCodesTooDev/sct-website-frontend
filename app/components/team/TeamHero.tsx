1a7b0b56eeb2a8cc9225425574fd5d1902608ac5
Raghad Hav <raghad.hav@gmail.com>
2024-09-29 16:16:32 +0100

    edit team page and other areas in the website

--- app/components/team/TeamHero.tsx
+++ app/components/team/TeamHero.tsx
@@ -1,31 +1,31 @@
-'use client';
-import YellowLine from '../common/YellowLine';
-import { motion } from 'framer-motion';
-export default function TeamHero() {
+"use client";
+import YellowLine from "../common/YellowLine";
+import { motion } from "framer-motion";
+export default function TeamHero({ title }: any) {
   return (
-    <div className='flex justify-center flex-col items-center'>
+    <div className="flex justify-center flex-col items-center">
       <motion.div
-        className='green-square-team'
+        className="green-square-team"
         initial={{ opacity: 0, y: -50 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: 'easeInOut' }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
         viewport={{ once: true }}
       ></motion.div>
 
       <motion.p
-        className='font-bold text-center text-4xl leading-loose text-sctblue'
+        className="font-bold text-center text-4xl leading-loose text-sctblue"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
+        transition={{ duration: 1, ease: "easeInOut" }}
         viewport={{ once: true }}
       >
-        Who is behind SheCodesToo;
+        {title}
       </motion.p>
 
       <motion.div
         initial={{ opacity: 0, scaleX: 0 }}
         whileInView={{ opacity: 1, scaleX: 1 }}
-        transition={{ duration: 0.8, ease: 'easeInOut' }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
         viewport={{ once: true }}
       >
         <YellowLine />
