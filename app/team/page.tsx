dd4f90819c1b62cf85bb55ca63c77097f6a5e3a5
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2024-10-22 11:30:10 +0300

    fix titles in teams

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import TeamHero from "../components/team/TeamHero";
-import TeamList from "../components/team/TeamList";
+import React from 'react';
+import TeamHero from '../components/team/TeamHero';
+import TeamList from '../components/team/TeamList';
 
 const Team = () => {
   return (
-    <div className="flex flex-col w-full justify-center items-center py-20 px-8 xl:px-32 text-start">
-      <TeamHero title=" Meet the Team;" />
+    <div className='flex flex-col w-full justify-center items-center py-20 px-8 xl:px-32 text-start'>
       <TeamList />
     </div>
   );
