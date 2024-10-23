0f8ccac718d9c820b68ded1856a1dbe68ec60eb6
Raghad Hav <raghad.hav@gmail.com>
2024-10-23 15:27:11 +0100

    add course detail

--- app/components/common/cards/CourseCard.tsx
+++ app/components/common/cards/CourseCard.tsx
@@ -1,63 +1,70 @@
-'use client';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import Link from 'next/link';
-import Tag from '@/app/assets/illustrations/html-tag.svg';
-import { truncateText } from '@/app/utils/truncateText';
-import Button from '../Button';
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import Tag from "@/app/assets/illustrations/html-tag.svg";
+import { truncateText } from "@/app/utils/truncateText";
+import Button from "../Button";
+import { useRouter } from "next/navigation";
 
-const CourseCard = ({ title, description, icon, isCourseOpen }: any) => {
+const CourseCard = ({ title, description, slug, isCourseOpen }: any) => {
   // Define the animation variants for the card
+  const router = useRouter();
+
   const cardVariants = {
     hidden: { opacity: 0, scale: 0.5 },
     visible: {
       opacity: 1,
       scale: 1,
-      transition: { duration: 1, ease: 'easeInOut' },
+      transition: { duration: 1, ease: "easeInOut" },
     },
   };
-
+  const handleClick = (e: any) => {
+    if (!isCourseOpen) {
+      e.preventDefault();
+    } else {
+      router.push(`/courses/${slug}`);
+    }
+  };
   return (
     <motion.div
-      className='border border-t-2 border-r-2 border-l-2 rounded-md shadow-xl flex py-2 px-6 justify-center'
-      initial='hidden'
-      animate='visible'
+      className="border border-t-2 border-r-2 border-l-2 rounded-md shadow-xl flex py-2 px-6 justify-center"
+      initial="hidden"
+      animate="visible"
       variants={cardVariants}
     >
-      <div className='my-5'>
+      <div className="my-5">
         <Image
-          blurDataURL='data:image/jpeg...'
-          className='w-10 h-10 object-contain mb-4'
+          blurDataURL="data:image/jpeg..."
+          className="w-10 h-10 object-contain mb-4"
           src={Tag}
-          alt='partner'
+          alt="partner"
         />
-        <div className='flex flex-col justify-start items-start'>
-          <p className='font-bold text-lg lg:text-xl mt-2 my-3'>{title}</p>
-          <div className='font-medium text-lg lg:text-xl mt-2 my-3'>
+        <div className="flex flex-col justify-start items-start">
+          <p className="font-bold text-lg lg:text-xl mt-2 my-3">{title}</p>
+          <div className="font-medium text-lg lg:text-xl mt-2 my-3">
             {description.map((block: any, index: any) => (
               <p
                 key={index}
-                className='text-start font-medium text-sm sm:text-md md:text-base w-full leading-loose text-secondary'
+                className="text-start font-medium text-sm sm:text-md md:text-base w-full leading-loose text-secondary"
               >
-                {block.type === 'paragraph' &&
-                  block.children.map((child: any, idx: any) => {
-                    return truncateText(child.text, 150);
-                  })}
+                {block.type === "heading" &&
+                  block.level === 6 &&
+                  block.children.map((child: any, idx: any) => child.text)}
               </p>
             ))}
           </div>
-          <Link className='mt-4' href={isCourseOpen ? 'news' : '#'} passHref>
-            <Button
-              extrStyles='text-xs sm:text-base text-black'
-              color={
-                isCourseOpen
-                  ? 'bg-sctyellow'
-                  : 'bg-gray-400 cursor-not-allowed text-white'
-              }
-            >
-              {isCourseOpen ? 'Join ->' : 'Applications are closed'}
-            </Button>
-          </Link>
+
+          <button
+            className={`${
+              isCourseOpen ? "bg-sctyellow" : "bg-gray-400 cursor-not-allowed"
+            } p-2 rounded-lg px-4 text-xl mt-4 text-black ${
+              isCourseOpen ? "hover:bg-sctblue hover:text-white" : "text-white"
+            }`}
+            onClick={handleClick}
+          >
+            {isCourseOpen ? "Join →" : "Applications are closed"}
+          </button>
         </div>
       </div>
     </motion.div>
---
+++ app/courses/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import { getSCTCourse } from "../../services/api";
+import YellowLine from "../../components/common/YellowLine";
+import Link from "next/link";
+import Button from "@/app/components/common/Button";
+const Course = async ({ params }: any) => {
+  const { slug } = params;
+  const course = await getSCTCourse(slug);
+
+  return (
+    <div className="sct-bg py-20 px-8 flex flex-col items-center w-full xl:px-32 text-start">
+      <div className="flex justify-center flex-col items-center">
+        <p className="font-bold text-center text-4xl leading-loose text-sctblue">
+          {course.title}
+        </p>
+
+        <div className="font-medium text-start text-base md:text-md mb-16 leading-loose text-black">
+          Data Analytics Program is a 8-month program designed to empower women
+          in the field of Data Analytics to start a career in this field. The
+          program offers the following:
+          <ul className="list-disc list-inside">
+            <li>Foundation of Data Analytics Training with Excel</li>
+            <li>Advanced Data Analytics Training with SQL and Python</li>
+            <li>Data Analytics in Business Training</li>
+            <li>Career Development Sessions</li>
+            <li>Mentorship</li>
+            <li>Capstone Project and Showcase Events</li>
+            <li>Job Placement Support</li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-4">
+          <Button color="bg-sctblue">
+            <a href="#">Read More Details Here</a>
+          </Button>
+          <Button color="bg-sctyellow">
+            <a href="https://forms.gle/FqRMDq6zkLUPWWfs8">Apply Now</a>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Course;
