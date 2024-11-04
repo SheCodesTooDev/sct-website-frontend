04cf70c25a531064449179c16bd3198134bdb3ba
Raghad Hav <raghad.hav@gmail.com>
2024-11-04 18:35:49 +0000

    change team photo

--- app/data/team.js
+++ app/data/team.js
@@ -1,50 +1,50 @@
-import Raghad from '../assets/sct/raghad.webp';
-import Ibrahim from '../assets/sct/ibrahim.webp';
-import Aya from '../assets/sct/aya.webp';
-import Patrick from '../assets/sct/patrick.webp';
-import Mohammed from '../assets/sct/mohammed.webp';
-import AyaSalam from '../assets/sct/ayasalam.webp';
-import Jaafar from '../assets/sct/jaafar.webp';
-import Athraa from '../assets/sct/athraa.webp';
-import AthraF from '../assets/sct/athraafaraj.webp';
-import Hawra from '../assets/sct/hawraa.webp';
-import Nawar from '../assets/sct/nawar.webp';
-import Yasir from '../assets/sct/yasir.webp';
-import Murt from '../assets/sct/murt.webp';
-import MDheya from '../assets/sct/hamdoon.webp';
-import Aida from '../assets/sct/aida.webp';
-import Ahmedamer from '../assets/sct/ahmed.webp';
-import Maha from '../assets/sct/maha.webp';
+import Raghad from "../assets/sct/raghad.webp";
+import Ibrahim from "../assets/sct/ibrahim.webp";
+import Aya from "../assets/sct/aya.webp";
+import Patrick from "../assets/sct/patrick.webp";
+import Mohammed from "../assets/sct/mohammed.webp";
+import AyaSalam from "../assets/sct/ayasalam.webp";
+import Jaafar from "../assets/sct/jaafar.webp";
+import Athraa from "../assets/sct/athraa.webp";
+import AthraF from "../assets/sct/athraafaraj.webp";
+import Hawra from "../assets/sct/hawraa.webp";
+import Nawar from "../assets/sct/nawar.webp";
+import Yasir from "../assets/sct/yasir.webp";
+import Murt from "../assets/sct/murt.webp";
+import MDheya from "../assets/sct/hamdoon.webp";
+import Aida from "../assets/sct/aida.webp";
+import Ahmedamer from "../assets/sct/ahmed.webp";
+import Maha from "../assets/sct/maha.webp";
 
 export const team = [
   {
     id: 1,
-    name: 'Raghad Al-Abboodi',
-    position: 'Founder and Director',
+    name: "Raghad Al-Abboodi",
+    position: "Founder and Director",
     image: Raghad,
   },
   {
     id: 2,
-    name: 'Ibrahim Jamal',
-    position: 'Project Manger',
+    name: "Ibrahim Jamal",
+    position: "Project Manger",
     image: Ibrahim,
   },
   {
     id: 3,
-    name: 'Aya Mohammed',
-    position: 'Project Assistant',
+    name: "Aya Mohammed",
+    position: "Project Assistant",
     image: Aya,
   },
   {
     id: 4,
-    name: 'Yasir Al-Samarai',
-    position: 'Finance Manger',
+    name: "Yasir Al-Samarai",
+    position: "Finance Manger",
     image: Yasir,
   },
   {
     id: 5,
-    name: 'Dr. Patrick Atkinson',
-    position: 'Sr. Software Engineer at Nvidia',
+    name: "Dr. Patrick Atkinson",
+    position: "Sr. Software Engineer at Nvidia",
     image: Patrick,
   },
 ];
@@ -52,44 +52,44 @@ export const team = [
 export const advisors = [
   {
     id: 6,
-    name: 'Murtadha Al-Tamimi',
-    position: 'Software Engineer at Meta',
+    name: "Murtadha Al-Tamimi",
+    position: "Software Engineer at Meta",
     image: Murt,
   },
   {
     id: 7,
-    name: 'Dr. Aida Al-Obaidi',
-    position: 'Assistant Lecturer',
+    name: "Dr. Aida Al-Obaidi",
+    position: "Assistant Lecturer",
     image: Aida,
   },
   {
     id: 8,
-    name: 'Maha Mahmood',
-    position: 'Assistant Lecturer',
+    name: "Maha Mahmood",
+    position: "Assistant Lecturer",
     image: Maha,
   },
   {
     id: 9,
-    name: 'Mohammed Dheyauldeen',
-    position: 'Data Analyst at Mercedes-Benz',
+    name: "Mohammed Dheyauldeen",
+    position: "Data Analyst at Mercedes-Benz",
     image: MDheya,
   },
   {
     id: 10,
-    name: 'Nawar Tamawi',
-    position: 'Sr. Software Engineer, Tableau at Salesforce',
+    name: "Nawar Tamawi",
+    position: "Sr. Software Engineer, Tableau at Salesforce",
     image: Nawar,
   },
   {
     id: 11,
-    name: 'Hawra Milani ',
-    position: 'Co-Founder / Director of the Code People',
+    name: "Hawra Milani ",
+    position: "Co-Founder / Director of the Code People",
     image: Hawra,
   },
   {
     id: 12,
-    name: 'Athraa Faraj',
-    position: 'Public Relation Support',
+    name: "Athraa Faraj",
+    position: "Public Relation Support",
     image: AthraF,
   },
 ];
@@ -97,32 +97,32 @@ export const advisors = [
 export const trainers = [
   {
     id: 13,
-    name: 'Mohammed Ali',
-    position: 'Trainer',
+    name: "Mohammed Ali",
+    position: "Trainer",
     image: Mohammed,
   },
   {
     id: 14,
-    name: 'Aya Salam',
-    position: 'Trainer',
+    name: "Aya Salam",
+    position: "Trainer",
     image: AyaSalam,
   },
   {
     id: 15,
-    name: 'Jaafar Mohammed',
-    position: 'Trainer Assistant',
+    name: "Jaafar Mohammed",
+    position: "Trainer Assistant",
     image: Jaafar,
   },
   {
     id: 16,
-    name: 'Athraa Mohammed',
-    position: 'Trainer Assistant',
+    name: "Athraa Mohammed",
+    position: "Trainer Assistant",
     image: Athraa,
   },
   {
     id: 17,
-    name: 'Ahmed Amer',
-    position: 'Trainer',
+    name: "Ahmed Amer",
+    position: "Trainer",
     image: Ahmedamer,
   },
 ];
